0ce07da9cf0ebab335782fa0d4887344849fd192
Armando De La Rosa <armando@getclue.com>
2023-10-29 19:49:41 -0600

    Added Vehicular Flux

--- mty.py
+++ mty.py
@@ -6,6 +6,8 @@ from threading import Thread, Lock
 from queue import Queue, Empty
 from sort import Sort
 import time
+import math
+import collections.abc
 
 class YOLOProcessor:
     def __init__(self):
@@ -28,6 +30,9 @@ class YOLOProcessor:
             self.classes = [line.strip() for line in f.readlines()]
 
         self.font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    def nms_boxes(self, boxes, scores, threshold):
+        return cv2.dnn.NMSBoxes(boxes, scores, score_threshold=0.1, nms_threshold=threshold)
         
     def detect_and_annotate(self, img):
         height, width, _ = img.shape
@@ -37,6 +42,8 @@ class YOLOProcessor:
 
         vehicle_classes = ['car', 'truck', 'bus', 'motorbike', 'bicycle', 'aeroplane']
         detected_boxes = []
+        confidences = []
+        class_ids = []
 
         for out in outs:
             for detection in out:
@@ -59,11 +66,26 @@ class YOLOProcessor:
                     y2 = y1 + h
 
                     detected_boxes.append([x1, y1, x2, y2])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
 
-                    color = (0, 255, 0)
-                    cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+        indices = self.nms_boxes(detected_boxes, confidences, 0.4)
+        final_indices = []  
 
-        return img, detected_boxes
+        for index in indices:
+            if isinstance(index, collections.abc.Iterable):
+                i = index[0]
+            else:
+                i = index
+
+            x1, y1, x2, y2 = detected_boxes[i]
+            label = self.classes[class_ids[i]]
+            color = (0, 255, 0)
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+            final_indices.append(i) 
+
+        return img, [detected_boxes[i] for i in final_indices]
+
     
 def get_youtube_video_url(youtube_url):
     ydl_opts = {
@@ -75,6 +97,7 @@ def get_youtube_video_url(youtube_url):
         return info_dict['url']
 
 class CaptureAndProcessThread(Thread):
+
     def __init__(self, video_url, queue, tracker, frame_lock):
         super().__init__()
         self.cap = cv2.VideoCapture(video_url)
@@ -82,10 +105,13 @@ class CaptureAndProcessThread(Thread):
         self.yolo_processor = YOLOProcessor()
         self.tracker = tracker
         self.lock = frame_lock
-        self.vehicle_count = 0
+        self.current_vehicle_ids = set()
+        self.vehicles_that_crossed = 0
+        self.persistent_detection = {}  
+        self.frame_width = int(self.cap.get(3))  
+        self.detection_gone_threshold = 3 
         self.last_time = time.time()
-        self.persistent_detection = {}  # Dictionary to track persistent detections
-        
+
     def run(self):
         frame_skip = 2
         frame_count = 0
@@ -98,43 +124,67 @@ class CaptureAndProcessThread(Thread):
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 continue
 
-            if frame_count % frame_skip == 0: 
+            if frame_count % frame_skip == 0:
                 frame = cv2.resize(frame, (640, 480))
                 annotated_frame, detected_boxes = self.yolo_processor.detect_and_annotate(frame)
 
-                trackers = self.tracker.update(np.array(detected_boxes))
+                current_detected_ids = []
+                if detected_boxes:
+                    trackers = self.tracker.update(np.array(detected_boxes))
+                    for track in trackers:
+                        tid = int(track[4])
+                        current_detected_ids.append(tid)
 
-                for track in trackers:
-                    tid = int(track[4])
-                    if tid not in self.persistent_detection:
-                        self.persistent_detection[tid] = 1
-                    else:
-                        self.persistent_detection[tid] += 1
+                        if tid not in self.persistent_detection:
+                            self.persistent_detection[tid] = {
+                                'bbox': track[:4], 
+                                'frames_since_detected': 0
+                            }
+                        else:
+                            self.persistent_detection[tid]['bbox'] = track[:4]
+                            self.persistent_detection[tid]['frames_since_detected'] = 0
 
-                    # Count car if it has been persistently detected in 3 or more frames
-                    if self.persistent_detection[tid] == 3:
-                        self.vehicle_count += 1
+                self.current_vehicle_ids.update(current_detected_ids)
+
+                for tid, data in list(self.persistent_detection.items()):
+                    if tid not in current_detected_ids:
+                        data['frames_since_detected'] += 1
+                        x1, y1, x2, y2 = data['bbox']
+
+                        touching_border = x1 <= 0 or y1 <= 0 or x2 >= self.frame_width or y2 >= frame.shape[0]
+
+                        if touching_border and data['frames_since_detected'] >= self.detection_gone_threshold:
+                            self.vehicles_that_crossed += 1
+                            del self.persistent_detection[tid]
 
                 if time.time() - self.last_time >= 60:
-                    print(f"Moving cars counted in last minute: {self.vehicle_count}")
-                    self.vehicle_count = 0
+                    print(f"Cars that crossed in the last minute: {self.vehicles_that_crossed}")
+                    print(f"Cars present during the last minute: {len(self.current_vehicle_ids)}")
+                    
+                    self.vehicles_that_crossed = 0
+                    self.current_vehicle_ids.clear()
                     self.last_time = time.time()
 
                 with self.lock:
                     if not self.queue.full():
                         self.queue.put(annotated_frame)
-                    
+
 app = Flask(__name__)
 
-video_url = get_youtube_video_url("https://www.youtube.com/embed/NRd9c9HSZSk")
+video_url = get_youtube_video_url("https://www.youtube.com/watch?v=6dp-bvQ7RWo")
 tracker = Sort()
 frame_lock = Lock()
 frame_queue = Queue(maxsize=86400)
 
-# Start the combined capture and processing thread
 capture_process_thread = CaptureAndProcessThread(video_url, frame_queue, tracker, frame_lock)
 capture_process_thread.start()
 
+# TODO IMPLEMENT 
+# Implement a database
+# Implement a newwer YOLO
+# WAIT UNTIL NEXT FRAME don't reset the video
+# Vehicular Flux Per Minute: This represents the number of cars that completely crossed the field of view in the video feed within a minute. It counts the vehicles that entered the frame from one side and exited from the other side within that minute.
+# Cars Present in a Given Minute: This represents the number of unique cars that were detected in the frame during a particular minute, regardless of whether they crossed completely or just partially appeared in the frame.
 @app.route('/video_feed')
 def video_feed():
     def generate():
